ffeecae904f53b13a6bbb6b0cb196897d4c43a9e
Jack David <jdavid3@wisc.edu>
2024-05-01 17:42:35 -0400

    Added numbers 1-10

--- HelloWorld/src/HelloWorld/HelloWorld.java
+++ HelloWorld/src/HelloWorld/HelloWorld.java
@@ -3,7 +3,10 @@ package HelloWorld;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		System.out.println("Hello Jack!");
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(i);
+		}
 	}
 
 }
